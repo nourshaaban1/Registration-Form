1d067b0f8549ea6abb9055bccbb76367aea45e84
Nour Shaaban <nourshaaban59@gmail.com>
2025-03-23 11:13:01 +0200

    Project Structure :)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name=" description" content="Registration Form">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="/assets/JS/API_Ops.js"></script>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <?php include 'footer.php'; ?>
+    <script src="script.js"></script>
+</body>
+
+</html>
